65b00ac3e2367e246ece0c6be256309851274ad9
ZombineDev <petar.p.kirov@gmail.com>
2017-11-03 00:55:57 +0200

    Add option for building in C++98 and C++11 modes
    Also clean-up the CircleCI script by using the existing one in scripts/.

--- scripts/build_from_scratch.sh
+++ scripts/build_from_scratch.sh
@@ -10,8 +10,11 @@ MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJ_DIR="$( cd "${MY_DIR}/../" && pwd )"
 BUILD_DIR="$PROJ_DIR/build"
 
-rm -rf "$BUILD_DIR"
-mkdir -v "$BUILD_DIR"
-cd $BUILD_DIR
-cmake ..
-make
+function build() {
+    rm -rf "$BUILD_DIR"
+    mkdir -v "$BUILD_DIR"
+    cmake -Bbuild -H. "$@"
+    cmake --build ./build
+}
+
+build "$@"
--- src/test_cmake.cpp
+++ src/test_cmake.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include "config/config.h"
 
+void printCppVer()
+{
+    using namespace std;
+#if __cplusplus >= 201103L
+    cout << "In C++11 mode!" << endl;
+#else
+    cout << "In C++98 mode!" << endl;
+#endif
+}
+
 int main()
 {
     using namespace std;
+    printCppVer();
     cout << "Library name: " << LIB_NAME << endl;
     cout << "Library version: " << LIB_VERSION << endl;
     cout << "Library description: " << LIB_DESCRIPTION << endl;
