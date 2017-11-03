4292a75a3a1bd82d6fa7bf4422d1687f3a9e3fc5
ZombineDev <petar.p.kirov@gmail.com>
2017-11-03 22:34:09 +0200

    Add pre-C++11 emulation for 'enum class' and 'static_assert'
    Add those as macros to image/config/config.h.in.
    Also add a test (test_enum_class) for them.

---
+++ tests/test_enum_class/main.cpp
@@ -0,0 +1,34 @@
+#include "image/config/config.h"
+#include "test_util/assert.h"
+
+ENUM_CLASS_BEGIN(Enum1, uint32_t)
+    member1,
+    member2,
+    member3 = 31,
+    member4
+ENUM_CLASS_END
+
+ENUM_CLASS_BEGIN(Enum2, uint8_t)
+    member1,
+ENUM_CLASS_END
+
+int main()
+{
+    STATIC_ASSERT(sizeof(Enum1) == 4);
+    STATIC_ASSERT((uint32_t)Enum1::member1 == 0);
+    STATIC_ASSERT((uint32_t)Enum1::member2 == 1);
+    STATIC_ASSERT((uint32_t)Enum1::member3 == 31);
+    STATIC_ASSERT((uint32_t)Enum1::member4 == 32);
+
+    STATIC_ASSERT(sizeof(Enum2) == 1);
+    STATIC_ASSERT((uint8_t)Enum2::member1 == 0);
+
+    ASSERT_EQ(sizeof(Enum1), 4);
+    ASSERT_EQ((uint32_t)Enum1::member1, 0);
+    ASSERT_EQ((uint32_t)Enum1::member2, 1);
+    ASSERT_EQ((uint32_t)Enum1::member3, 31);
+    ASSERT_EQ((uint32_t)Enum1::member4, 32);
+
+    ASSERT_EQ(sizeof(Enum2), 1);
+    ASSERT_EQ((uint8_t)Enum2::member1, 0);
+}
