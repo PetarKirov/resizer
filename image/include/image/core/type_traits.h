1fcd91223fce3e759198673960f5c4f7af1d1a14
ZombineDev <petar.p.kirov@gmail.com>
2017-11-09 13:45:27 +0200

    Add image/core/type_traits.h
    Adds `enable_if<bool, T>`, `is_same<T, U>` and `remove_const<T>` meta functions.

---
+++ image/include/image/core/type_traits.h
@@ -0,0 +1,25 @@
+#ifndef IMAGE_CORE_TYPE_TRAITS_H
+#define IMAGE_CORE_TYPE_TRAITS_H
+
+namespace image
+{
+namespace core
+{
+    template<bool B, class T = void>
+    struct enable_if {};
+
+    template<class T>
+    struct enable_if<true, T> { typedef T type; };
+
+    template<class T, class U>
+    struct is_same { enum { value = 0 }; };
+
+    template<class T>
+    struct is_same<T, T> { enum { value = 1 }; };
+
+    template<class T> struct remove_const          { typedef T type; };
+    template<class T> struct remove_const<const T> { typedef T type; };
+}
+}
+
+#endif /* IMAGE_CORE_TYPE_TRAITS_H */
